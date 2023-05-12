d87151ac1c6e081790a9eb8dcc1d61c5435a638e
antoprince001 <antoprince001@gmail.com>
2023-05-12 16:31:30 +0530

    initial project setup

---
+++ src/main/java/com/retreat/copilot/controller/AuthenticationController.java
@@ -0,0 +1,8 @@
+package com.retreat.copilot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+}
---
+++ src/test/java/com/retreat/copilot/CopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retreat.copilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
